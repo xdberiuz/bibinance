c586ac6845ffd326790bd197bf103172fbb190ea
xdberiuz <xdvooddoo@gmail.com>
2022-06-16 15:12:20 +0300

    responsive tablet mobile

--- src/steps/FinalStep.vue
+++ src/steps/FinalStep.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="section">
-        <div class="warper">
+    <div class="section__final">
+        <div class="section__left">
             <h1 class="final__title">Благодарим Вас
                 за регистрацию!</h1>
             <h2 class="final_subtitle">Ваша заявка на участие в инвестиционном проекте Halyk Finance принята.</h2>
-            <div class="final__img">
+            <div class="final__mobImg">
                 <img src="../assets/img/finalStep.png" alt="Успешная регистрация">
             </div>
             <div class="final__text">В ближайшее время Вы получите выгодное предложение от лидера рынка инвестицый.
@@ -12,6 +12,9 @@
             <div class="final__text">Спасибо за доверие к нашей команде.
                 Halyk Finance строитсветлое будущее с лидерами! </div>
             <router-link class="final__btn" to="/bibinance/"><span>На главную</span></router-link>
+        </div>
+        <div class="section__desktop">
+            <img src="../assets/img/finalStep.png" alt="">
         </div>
     </div>
 </template>
@@ -25,15 +28,31 @@ export default {
     }
 }
 </script>
-<style scoped>
-.section {
+<style>
+#app {
+    padding: 0;
+}
+
+.final__mobImg {
+    display: none;
+}
+
+.section__final {
     position: relative;
     padding-left: 40px;
     padding-top: 40px;
-    width: 1200px;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    max-width: unset;
+    padding: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #fff;
 }
 
-.warper {
+.section__left {
     width: 480px;
     height: 400px;
     display: flex;
@@ -83,5 +102,72 @@ export default {
     text-decoration: none;
     display: flex;
     justify-content: center;
+}
+
+@media screen and (max-width: 1250px) {
+    .section__desktop img {
+        width: 500px;
+    }
+}
+
+@media screen and (max-width: 1031px) {
+    .section__desktop img {
+        width: 400px;
+    }
+
+    .final__title {
+        font-size: 36px;
+    }
+
+    .final_subtitle {
+        font-size: 20px;
+    }
+
+    .final__text {
+        font-size: 16px;
+    }
+}
+
+@media screen and (max-width: 847px) {
+    .section__final {
+        height: 100vh;
+        padding: 0;
+    }
+
+    .section__desktop {
+        display: none;
+    }
+
+    .final__mobImg {
+        display: block;
+    }
+
+    .section__left {
+        max-width: unset;
+        width: unset;
+        height: unset;
+        align-items: center;
+        justify-content: space-between;
+        height: inherit;
+        padding: 50px 0 50px 0;
+    }
+
+    .final__text,
+    .final__title,
+    .final_subtitle {
+        text-align: center;
+        padding: 0 10px 0 10px;
+    }
+
+    img {
+        width: 450px;
+    }
+}
+
+@media screen and (max-width: 450px) {
+    img {
+        width: 320px;
+    }
+    
 }
 </style>
