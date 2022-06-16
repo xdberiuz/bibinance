9a665c95d62c2e303a62c38fc25227b64334e043
xdberiuz <xdvooddoo@gmail.com>
2022-06-16 11:39:26 +0300

    problem with path

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  filenameHashing: false,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production'? './' : '/'
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/docs/" : "/",
 })
 
 
